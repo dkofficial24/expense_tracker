e0fcc4a66c92c672c289ad44a5478843c346c970
Dinesh Kumar <dkofficial24@gmail.com>
2023-05-07 21:14:00 +0530

    - screen classes added

---
+++ lib/expense_tracker/ui/screens/create_expense_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateExpenseScreen extends StatefulWidget {
+  const CreateExpenseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreateExpenseScreen> createState() => _CreateExpenseScreenState();
+}
+
+class _CreateExpenseScreenState extends State<CreateExpenseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/expense_tracker/ui/screens/view_expense_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewExpenseScreen extends StatefulWidget {
+  const ViewExpenseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ViewExpenseScreen> createState() => _ViewExpenseScreenState();
+}
+
+class _ViewExpenseScreenState extends State<ViewExpenseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
