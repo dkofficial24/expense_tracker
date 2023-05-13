01aac2fbf365c6bbc528cc242342a7fd0c0b2c98
Dinesh Kumar <dkofficial24@gmail.com>
2023-05-13 20:14:36 +0530

    - categoryItem and expenseInfo model added

--- lib/expense_tracker/models/expense_info_model.dart
+++ lib/expense_tracker/models/expense_info_model.dart
@@ -1,3 +1,19 @@
-class ExpenseInfo {
+class ExpenseCategoryInfo {
+  double amount;
+  String category;
 
+  ExpenseCategoryInfo(this.amount, this.category);
+}
+
+class ExpenseInfo {
+  List<ExpenseCategoryInfo> categoryData;
+  int total;
+  int id;
+  int date;
+
+  ExpenseInfo(
+      {required this.id,
+      required this.categoryData,
+      required this.total,
+      required this.date});
 }
