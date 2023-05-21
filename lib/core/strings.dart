f0e0d9ccce6e6d78925db224c549ec0627737638
Dinesh Kumar <dkofficial24@gmail.com>
2023-05-21 14:03:36 +0530

    Refactor: Centralize string constants in strings.dart for enhanced code organization

---
+++ lib/core/strings.dart
@@ -0,0 +1,10 @@
+class StringConstant {
+  static const String party = "Party";
+  static const String movie = "Movie";
+  static const String trip = "Trip";
+  static const String club = "Club";
+  static const String fruits = "Fruits";
+  static const String total = "Total";
+  static const String reset = "Reset";
+  static const String save = "Save";
+}
