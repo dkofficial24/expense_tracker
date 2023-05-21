f3ae8ec23f278a0daf640edbcefd7e8217a17ac5
Dinesh Kumar <dkofficial24@gmail.com>
2023-05-21 14:06:12 +0530

    Refactor: Extract inbuilt onTap function into separate functions for improved code modularity Feat: Introduce reset and save buttons for enhanced user interaction

--- lib/expense_tracker/ui/screens/create_expense_screen.dart
+++ lib/expense_tracker/ui/screens/create_expense_screen.dart
@@ -1,5 +1,7 @@
+import 'package:expense_tracker/core/core.dart';
 import 'package:expense_tracker/expense_tracker/models/category_item.dart';
-import 'package:expense_tracker/expense_tracker/ui/widgets/custom_text_field.dart';
+import 'package:expense_tracker/expense_tracker/ui/create_expense.util.dart';
+import 'package:expense_tracker/expense_tracker/ui/ui.dart';
 import 'package:flutter/material.dart';
 
 class CreateExpenseScreen extends StatefulWidget {
@@ -17,11 +19,11 @@ class _CreateExpenseScreenState extends State<CreateExpenseScreen> {
   bool isCategorySelected = false;
 
   Map<String, CategoryItem> itemMap = {
-    "Party": CategoryItem(),
-    "Movie": CategoryItem(),
-    "Trip": CategoryItem(),
-    "Club": CategoryItem(),
-    "Fruits": CategoryItem(),
+    StringConstant.party: CategoryItem(),
+    StringConstant.movie: CategoryItem(),
+    StringConstant.trip: CategoryItem(),
+    StringConstant.club: CategoryItem(),
+    StringConstant.fruits: CategoryItem(),
   };
 
   CategoryItem? currentCategoryItem;
@@ -39,7 +41,7 @@ class _CreateExpenseScreenState extends State<CreateExpenseScreen> {
             mainAxisSize: MainAxisSize.min,
             children: [
               Text(
-                totalAmount.toString(),
+                '${StringConstant.party} - $totalAmount',
                 textAlign: TextAlign.start,
                 style:
                     const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
@@ -50,7 +52,12 @@ class _CreateExpenseScreenState extends State<CreateExpenseScreen> {
               Wrap(
                 children: itemMap.keys.map((e) {
                   itemMap[e]?.name = e;
-                  return _renderChip(itemMap[e]!);
+                  return CategoryChipWidget(
+                    categoryItem: itemMap[e]!,
+                    categoryTapCallback: (CategoryItem item) {
+                      onCategoryTap(item);
+                    },
+                  );
                 }).toList(),
               ),
               const SizedBox(
@@ -61,6 +68,7 @@ class _CreateExpenseScreenState extends State<CreateExpenseScreen> {
                   child: CustomTextFieldWidget(
                     controller: _amountEditingController,
                     isEnabled: currentCategoryItem != null,
+                    onBackspace: onBackButtonTap,
                     onChanged: (value) {
                       if (value.isEmpty) return;
                       currentCategoryItem?.amount = int.parse(value);
@@ -72,31 +80,36 @@ class _CreateExpenseScreenState extends State<CreateExpenseScreen> {
               ),
               Flexible(
                 child: GridView.count(
-                  crossAxisCount: 5,
+                  crossAxisCount: 3,
                   shrinkWrap: true,
-                  childAspectRatio: 1,
+                  childAspectRatio: 2,
                   children: List.generate(10, (index) {
                     return Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: ElevatedButton(
                           onPressed: currentCategoryItem != null
                               ? () {
-                                  if (_amountEditingController.text == '0') {
-                                    _amountEditingController.clear();
-                                  }
-                                  _amountEditingController.text +=
-                                      index.toString();
-                                  currentCategoryItem?.amount =
-                                      int.parse(_amountEditingController.text);
-                                  totalAmount +=
-                                      currentCategoryItem?.amount ?? 0;
-                                  setState(() {});
-                                  setCursorPosition();
+                                  onNumberTap(index);
                                 }
                               : null,
                           child: Text(index.toString())),
                     );
                   }),
+                ),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                        onPressed: resetData,
+                        child: const Text(StringConstant.reset)),
+                    ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(StringConstant.save)),
+                  ],
                 ),
               )
             ],
@@ -106,37 +119,56 @@ class _CreateExpenseScreenState extends State<CreateExpenseScreen> {
     );
   }
 
+  ///-------------------Functionalities Methods---------------------------------
+
+  void onCategoryTap(CategoryItem item) {
+    _amountEditingController.clear();
+    currentCategoryItem = item;
+    itemMap.forEach((key, value) {
+      if (key != currentCategoryItem?.name) {
+        value.isSelected = false;
+      } else {
+        value.isSelected = true;
+      }
+    });
+    _amountEditingController.text = item.amount.toString();
+    setState(() {});
+  }
+
+  void onNumberTap(int index) {
+    if (_amountEditingController.text == '0') {
+      _amountEditingController.clear();
+    }
+    _amountEditingController.text += index.toString();
+    currentCategoryItem?.amount = int.parse(_amountEditingController.text);
+
+    totalAmount = 0;
+    totalAmount = CreateExpenseUtil.calculateTotalAmount(itemMap);
+    setState(() {});
+    setCursorPosition();
+  }
+
+  onBackButtonTap() {
+    setState(() {
+      totalAmount = CreateExpenseUtil.calculateTotalAmount(itemMap);
+    });
+  }
+
+  void resetData() {
+    setState(() {
+      _amountEditingController.clear();
+      totalAmount = 0;
+      itemMap.forEach((key, value) {
+        value.amount = 0;
+        value.isSelected = false;
+      });
+    });
+  }
+
   void setCursorPosition() {
     _amountEditingController.selection =
         TextSelection.fromPosition(TextPosition(
       offset: _amountEditingController.text.length,
     ));
-  }
-
-  Widget _renderChip(CategoryItem item) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 4.0),
-      child: ElevatedButton(
-        onPressed: () {
-          _amountEditingController.clear();
-          currentCategoryItem = item;
-          itemMap.forEach((key, value) {
-            if (key != currentCategoryItem?.name) {
-              value.isSelected = false;
-            } else {
-              value.isSelected = true;
-            }
-          });
-          _amountEditingController.text = item.amount.toString();
-          setState(() {});
-        },
-        child: Text(
-          item.name,
-          style: TextStyle(
-            color: item.isSelected ? Colors.red : Colors.white,
-          ),
-        ),
-      ),
-    );
   }
 }
